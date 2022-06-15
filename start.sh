6d669e636d30893cd60fd0e4d1da020e8c4be330
Sjsjssjsjsjsjsj <107046063+Sjsjssjsjsjsjsj@users.noreply.github.com>
2022-06-15 10:03:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
+git clone https://github.com/Sjsjssjsjsjsjsj/radiov3 /radiov3
 echo "Installing Requirements..."
-cd /RadioPlayerV3
+cd /radiov3
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
