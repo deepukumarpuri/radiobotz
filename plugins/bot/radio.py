7937d9988fa949d81a2b571e275074325cff9ce0
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2022-06-15 10:41:51 +0530

    Update radio.py

--- plugins/bot/radio.py
+++ plugins/bot/radio.py
@@ -50,6 +50,7 @@ async def radio(_, message: Message):
     await mp.delete(k)
     await mp.delete(message)
 
+
 @Client.on_message(filters.command(["stopradio", f"stopradio@{USERNAME}"]) & ADMINS_FILTER & (filters.chat(CHAT_ID) | filters.private | filters.chat(LOG_GROUP)))
 async def stop(_, message: Message):
     if 0 in RADIO:
@@ -58,6 +59,31 @@ async def stop(_, message: Message):
         await mp.delete(message)
         return
     await mp.stop_radio()
+    k=await message.reply_text(f"{emoji.CROSS_MARK_BUTTON} **Radio Stream Ended Successfully!**")
+    await mp.delete(k)
+    await mp.delete(message)
+
+@Client.on_message(filters.command(["radios", f"radios@{USERNAME}"]) & ADMINS_FILTER & (filters.chat(DK_CHAT_ID) | filters.private | filters.chat(LOG_GROUP)))
+async def radio(_, message: Message):
+    if 1 in RADIO:
+        k=await message.reply_text(f"{emoji.ROBOT} **Please Stop Existing Radio Stream!**")
+        await mp.delete(k)
+        await message.delete()
+        return
+    await mp.dk_start_radio()
+    k=await message.reply_text(f"{emoji.CHECK_MARK_BUTTON} **Radio Stream Started :** \n<code>{STREAM}</code>")
+    await mp.delete(k)
+    await mp.delete(message)
+
+
+@Client.on_message(filters.command(["stopradios", f"stopradios@{USERNAME}"]) & ADMINS_FILTER & (filters.chat(DK_CHAT_ID) | filters.private | filters.chat(LOG_GROUP)))
+async def stop(_, message: Message):
+    if 0 in RADIO:
+        k=await message.reply_text(f"{emoji.ROBOT} **Please Start A Radio Stream First!**")
+        await mp.delete(k)
+        await mp.delete(message)
+        return
+    await mp.dk_stop_radio()
     k=await message.reply_text(f"{emoji.CROSS_MARK_BUTTON} **Radio Stream Ended Successfully!**")
     await mp.delete(k)
     await mp.delete(message)
